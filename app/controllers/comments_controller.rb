9f45e7b7abe942eb2a92eb4b0f4f1d14819f1cc4
John Diego <johnoliveirasilva9@gmail.com>
2020-12-24 18:19:58 -0300

    Refactor, Rendering Partial Collections

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,12 @@ class CommentsController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
+
+    if @comment.save
+      redirect_to @article
+    else
+      render :new
+    end
   end
 
   private
